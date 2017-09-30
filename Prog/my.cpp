8774e9e5f3a579ff1d802fd8d204270b70765635
chakradeokaustubh@gmail.com <chakradeokaustubh@gmail.com>
2017-09-30 11:39:45 +0530

    Intitial commit for C prerequisite programs

---
+++ Prog/my.cpp
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int mymain(int argc, char **argv, char **envp)
+{
+	int i,j;
+	cout<<"Command line arg count : "<<argc<<endl;
+	cout<<"Command line args "<<endl;
+	for(int i=0;i<argc;i++)
+	{
+		cout<<"\tArg "<<i + 1<<" : "<< argv[i]<<endl;
+		//(unsigned int)argv[i] << ", " <<argv[i]<<endl;
+		printf("Arg :%d Address : %s arg:%s\n", i+1, argv[i],argv[i]);
+	}
+	j=1;
+	while((*envp))
+	{
+		cout<<"Environment Variables : "<<*envp<<endl;
+		envp++;
+		j++;
+		cout<<j<<endl;
+	}
+	char *user = getenv("USER");
+	if(user)
+	{
+		cout<<"Username = "<<user<< ""<<endl;
+	}
+	else
+		cout<<"Error";
+	return 0;
+}
